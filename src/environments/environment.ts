b872343375abc19ee4db4ee03ead49a8576762f6
Thuc Huyen <thuctonhuyen@gmail.com>
2021-02-09 10:47:53 -0800

    change apiHost to match with AWS exposed NGINX url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://http://a7c998467539e4601bd2fd76f672d2f2-37364772.us-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
